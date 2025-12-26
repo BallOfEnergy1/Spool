900c32580c306688fa0098895eaf9e8b1028a05c
BallOfEnergy01 <66693744+BallOfEnergy1@users.noreply.github.com>
2025-12-26 08:39:35 -0600

    Change AE2 fix to be more general, targeting `FMLCommonHandler` rather than `Platform`. Fix NTM crash with the end (and potentially other dimensions) during event fired on ore generation.

--- src/main/java/com/gamma/spool/mixin/minecraft/FMLCommonHandlerMixin.java
+++ src/main/java/com/gamma/spool/mixin/minecraft/FMLCommonHandlerMixin.java
@@ -3,6 +3,7 @@ package com.gamma.spool.mixin.minecraft;
 import java.util.List;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 
 import com.gamma.spool.Tags;
@@ -11,6 +12,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.IFMLSidedHandler;
+import cpw.mods.fml.relauncher.Side;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 
 @Mixin(value = FMLCommonHandler.class, remap = false)
@@ -29,5 +31,28 @@ public abstract class FMLCommonHandlerMixin {
         List<String> additional = new ObjectArrayList<>(original.call(instance));
         additional.add(String.format("Spool %s", Tags.VERSION));
         return additional;
+    }
+
+    /**
+     * @author BallOfEnergy01
+     * @reason Fix improper thread analysis.
+     */
+    @Overwrite(remap = false)
+    public Side getEffectiveSide() {
+        Thread thread = Thread.currentThread();
+        if (thread.getName()
+            .toLowerCase()
+            .contains("server thread")) {
+            return Side.SERVER;
+        } else if (thread.getName()
+            .toLowerCase()
+            .contains("client thread")) {
+                return Side.CLIENT;
+            } else if (thread.getName()
+                .toLowerCase()
+                .contains("spool")) {
+                    return Side.SERVER;
+                }
+        return Side.CLIENT;
     }
 }
