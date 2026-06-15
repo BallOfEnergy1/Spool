5569ae2d2d30a1e7557b097053ed68cf0cf60815
BallOfEnergy01 <66693744+BallOfEnergy1@users.noreply.github.com>
2026-06-15 04:27:04 -0500

    cleanup and deleting the test suite because it pissed me off and slowed down my builds :grrr:

--- src/main/java/com/gamma/spool/asm/SpoolNames.java
+++ src/main/java/com/gamma/spool/asm/SpoolNames.java
@@ -15,23 +15,6 @@ public class SpoolNames {
 
         public static final String EMPTY_CHUNK = "net/minecraft/world/chunk/EmptyChunk";
         public static final String EMPTY_CHUNK_OBF = "apw";
-
-        public static final String EBS = "net/minecraft/world/chunk/storage/ExtendedBlockStorage";
-        public static final String EBS_OBF = "apz";
-
-        public static final String ANVIL_CHUNK_LOADER = "net/minecraft/world/chunk/storage/AnvilChunkLoader";
-        public static final String ANVIL_CHUNK_LOADER_OBF = "aqk";
-
-        public static final String CHUNK_PROVIDER_SERVER = "net/minecraft/world/gen/ChunkProviderServer";
-        public static final String CHUNK_PROVIDER_SERVER_OBF = "ms";
-        public static final String CHUNK_PROVIDER_FLAT = "net/minecraft/world/gen/ChunkProviderFlat";
-        public static final String CHUNK_PROVIDER_FLAT_OBF = "aqu";
-        public static final String CHUNK_PROVIDER_GENERATE = "net/minecraft/world/gen/ChunkProviderGenerate";
-        public static final String CHUNK_PROVIDER_GENERATE_OBF = "aqz";
-        public static final String CHUNK_PROVIDER_HELL = "net/minecraft/world/gen/ChunkProviderHell";
-        public static final String CHUNK_PROVIDER_HELL_OBF = "aqv";
-        public static final String CHUNK_PROVIDER_END = "net/minecraft/world/gen/ChunkProviderEnd";
-        public static final String CHUNK_PROVIDER_END_OBF = "ara";
     }
 
     public static class Destinations {
